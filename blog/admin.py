2baf4cbd6c11ac61f0f6fdf3e39f762759074247
Shreyas_Shetty <shreyasshetty788@gmail.com>
2020-03-07 23:43:37 +0530

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Instructor
+from .models import Course
+from .models import Student
+
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Instructor)
---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+
+class Course(models.Model) :
+    name = models.CharField(max_length=20, default='')
+    course_code = models.CharField(max_length=10, default='')
+
+    def publish(self) :
+        self.save()
+
+    def __str__(self) :
+        return self.name
+
+
+class Student(models.Model) :
+    name = models.CharField(max_length=20, default='')
+    rollno = models.CharField(max_length=10, unique=True, default='')
+    course_name = models.ForeignKey(Course, models.SET_NULL, blank=True, null=True)
+    email_id = models.CharField(max_length=40, default='')
+
+    def publish(self) :
+        self.save()
+
+    def __str__(self) :
+        return self.name
+
+
+class Instructor(models.Model) :
+    name = models.CharField(max_length=20, default='')
+    course_name = models.ForeignKey(Course, models.SET_NULL, blank=True, null=True)
+
+    def publish(self) :
+        self.save()
+
+    def __str__(self) :
+        return self.name
