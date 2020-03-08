d4707011c250171f697a96c4606d30a96f40f501
Shreyas_Shetty <shreyasshetty788@gmail.com>
2020-03-08 21:52:40 +0530

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Course
+from .models import Instructor
+from .models import Student
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+
+def post_list(request) :
+    courses = Course.objects.order_by('name')
+    return render(request, 'blog/post_list.html', {'courses' : courses})
