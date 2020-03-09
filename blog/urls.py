b9050f0be179068baa3f4518be3f6867cad1835a
Shreyas_Shetty <shreyasshetty788@gmail.com>
2020-03-09 18:38:13 +0530

    Sixth commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('course/<int:pk>/', views.course_detail, name='course_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 from .models import Instructor
 from .models import Student
@@ -7,3 +7,7 @@ from .models import Student
 def post_list(request) :
     courses = Course.objects.order_by('name')
     return render(request, 'blog/post_list.html', {'courses' : courses})
+
+def course_detail(request, pk) :
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'blog/course_detail.html', {'course' : course})
