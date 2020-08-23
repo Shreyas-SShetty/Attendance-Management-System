6d68e6a45435cc377514e01e117819e4ede4a830
shreyas_shetty <shreyasshetty788@gmail.com>
2020-08-23 18:24:43 +0530

    Modified Course Instructor relation

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.home_page, name='home_page'),
     path('course/<int:pk>/', views.course_detail, name='course_detail'),
     path('student_list/<int:pk>/', views.student_list, name='student_list'),
     path('take/<int:pk>/', views.take, name='take'),
--- blog/views.py
+++ blog/views.py
@@ -9,45 +9,16 @@ from .models import Instructor
 from .models import Attendance
 from .models import Student
 
-
-def post_list(request) :
+def home_page(request) :
     courses = Course.objects.order_by('name')
-    return render(request, 'blog/post_list.html', {'courses' : courses})
-
-
-def status(request) :
-    courses = Course.objects.order_by('name')
-    return render(request, 'blog/att_status.html', {'courses' : courses})
-
-
-def course_stat(request, pk) :
-    course = get_object_or_404(Course, pk=pk)
-    attendances = Attendance.objects.filter(course_name__name=course)
-    attendance_count = attendances.count()
-    return render(request, 'blog/particular_attd_status.html',
-                  {'attendances' : attendances, 'attendance_count' : attendance_count, 'course' : course})
-
-
-def student_stat(request, pk1, pk2) :
-    course__name = get_object_or_404(Course, pk=pk2)
-    student__name = get_object_or_404(Student, pk=pk1)
-    attendances = Attendance.objects.filter(course_name__name=course__name, student_name__name=student__name)
-    classes = Attendance.objects.filter(course_name__name=course__name)
-    class_count = ((attendances.count() * 100) / classes.count())
-    student_count = attendances.count()
-    return render(request, 'blog/particular_student_status.html',
-                  {'student_count' : student_count, 'class_count' : class_count})
-
-
-def course_detail(request, pk) :
-    course = get_object_or_404(Course, pk=pk)
-    return render(request, 'blog/course_detail.html', {'course' : course})
+    return render(request, 'blog/home_page.html', {'courses' : courses})
 
 
 @login_required
 def take(request, pk) :
     user_name = get_object_or_404(User, pk=pk)
     teacher = Instructor.objects.get(name=user_name)
+    # can't directly assign user_name to intructor_name of course because user_name is not an instance of instructor.
     all_courses = Course.objects.filter(instructor_name=teacher)
     return render(request, 'blog/take.html', {'all_courses' : all_courses})
 
@@ -62,10 +33,40 @@ def student_list(request, pk) :
             form.instance.instructor_name = course.instructor_name
             form.instance.published_date = timezone.now()
             form.save()
-            return redirect('post_list')
+            return redirect('home_page')
     else :
         form = PostForm(course=course)
     return render(request, 'blog/student_list.html', {'course' : course, 'form' : form})
+
+
+def status(request) :
+    courses = Course.objects.order_by('name')
+    return render(request, 'blog/att_status.html', {'courses' : courses})
+
+
+def course_stat(request, pk) :
+    course = get_object_or_404(Course, pk=pk)
+    attendance_object = Attendance.objects.filter(course_name__name=course)
+    attendance_count = attendance_object.count()
+    return render(request, 'blog/particular_attd_status.html',
+                  {'attendance_object' : attendance_object, 'attendance_count' : attendance_count, 'course' : course})
+
+
+def student_stat(request, pk1, pk2) :
+    course__name = get_object_or_404(Course, pk=pk2)
+    student__name = get_object_or_404(Student, pk=pk1)
+    attendances = Attendance.objects.filter(course_name__name=course__name, student_name__name=student__name)
+    classes = Attendance.objects.filter(course_name__name=course__name)
+    # or could have obtained this (classes) from particular_attd_status.html.
+    class_count = ((attendances.count() * 100) / classes.count())
+    student_count = attendances.count()
+    return render(request, 'blog/particular_student_status.html',
+                  {'student_count' : student_count, 'class_count' : class_count})
+
+
+def course_detail(request, pk) :
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'blog/course_detail.html', {'course' : course})
 
 
 def register(request) :
@@ -73,7 +74,7 @@ def register(request) :
         form = UserCreationForm(request.POST)
         if form.is_valid() :
             form.save()
-            return redirect('post_list')
+            return redirect('home_page')
     else :
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form' : form})
